10472ca02afaf498f71baf8f90ae7f1e9326bba6
Jinsung Ha <jsha@dyn1172-96.wlan.ic.ac.uk>
2017-11-09 12:30:39 +0000

    Gathered informations regarding topics.

---
+++ ODB/Yelp.hpp
@@ -0,0 +1,25 @@
+#include <memory>
+#include <odb/core.hxx>
+#include <odb/lazy-ptr.hxx>
+#include <set>
+#include <string>
+
+#pragma db view
+class StarCount{
+public:
+	int stars;
+	int count;
+};
+
+#pragma db view query("select top 1 text, last_elapsed_time from sys.dm_exec_query_stats cross apply sys.dm_exec_sql_text(sql_handle) order by last_execution_time desc")
+class LastQueryTime{
+public:
+	std::string text;
+	long elapsed_time;
+};
+
+// ---------------------------------------------
+// No need to change anything above this line
+// ---------------------------------------------
+
+
