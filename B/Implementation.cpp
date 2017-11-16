3f785db33981c52d38f2640ea896de07adb44c0d
Jinsung Ha <jsha@dyn1222-199.wlan.ic.ac.uk>
2017-11-16 16:39:53 +0000

    Implemented Nested Loop Join and Hash Join.

--- B/Implementation.cpp
+++ B/Implementation.cpp
@@ -2,50 +2,66 @@
 #include <iostream>
 
 using namespace std;
+
 //////////////////// Nested Loop Joins ////////////////////
 
-std::vector<std::string> getQualifyingBusinessesIDsVector(Businesses const& b, float latMin,
-																													float latMax, float longMin,
-																													float longMax) {
-	// This function needs to find all businesses that have within the
-	// specified latitude/longitude range and store their ids in the result vector
-	std::cout << "function getQualifyingBusinessesIDsVector not implemented" << std::endl;
-	throw std::logic_error("unimplemented");
+std::vector<std::string> getQualifyingBusinessesIDsVector(Businesses const& b, float latMin, float latMax, float longMin, float longMax) {
+
+	std::vector<std::string> result;
+	std::string currId;
+	float currLat, currLong;
+	for (int i = 0; i < b.ids.size(); i++) {
+		currId = b.ids[i];
+		currLat = b.latitudes[i];
+		currLong = b.longitudes[i];
+
+		if (currLat >= latMin && currLat <= latMax &&
+			currLong >= longMin && currLong <= longMax)
+			result.push_back(currId);
+	}
+
+	return result;
 }
 
 std::vector<unsigned long>
 performNestedLoopJoinAndAggregation(Reviews const& r, std::vector<std::string> const& qualifyingBusinessesIDs) {
-	// The second parameter of this function is the vector of qualifying
-	// business ids you have created in the first function
 
-	// This function needs to find all reviews that have business_ids in
-	// the qualifyingBusinessesIDs vector and build a histogram over their stars
-	// The return value is that histogram
+	std::vector<unsigned long> result = {0, 0, 0, 0, 0, 0};
+	for (int i = 0; i < r.business_ids.size(); i++)
+		for (int j = 0; j < qualifyingBusinessesIDs.size(); j++)
+			if (r.business_ids[i] == qualifyingBusinessesIDs[j])
+				result[r.stars[i]] += 1;
 
-	std::cout << "function performNestedLoopJoinAndAggregation not implemented" << std::endl;
-	throw std::logic_error("unimplemented");
+	return result;
 }
 
 //////////////////// Hash Join ////////////////////
 
-std::unordered_set<std::string> getQualifyingBusinessesIDs(Businesses const& b, float latMin,
-																													 float latMax, float longMin,
-																													 float longMax) {
-	// This function needs to find all businesses that have within the
-	// specified latitude/longitude range and store their ids in the result set
-	std::cout << "function getQualifyingBusinessesIDs not implemented" << std::endl;
-	throw std::logic_error("unimplemented");
+std::unordered_set<std::string> getQualifyingBusinessesIDs(Businesses const& b, float latMin, float latMax, float longMin, float longMax) {
+
+	std::unordered_set<std::string> result;
+	std::string currId;
+	float currLat, currLong;
+	for (int i = 0; i < b.ids.size(); i++) {
+		currId = b.ids[i];
+		currLat = b.latitudes[i];
+		currLong = b.longitudes[i];
+		
+		if (currLat >= latMin && currLat <= latMax &&
+			currLong >= longMin && currLong <= longMax)
+			result.insert(currId);
+	}
+
+	return result;
 }
 
 std::vector<unsigned long>
-aggregateStarsOfQualifyingBusinesses(Reviews const& r,
-																		 std::unordered_set<std::string> const& qualifyingBusinesses) {
-	// The second parameter of this function is the set of qualifying
-	// business ids you have created in the first function
+aggregateStarsOfQualifyingBusinesses(Reviews const& r, std::unordered_set<std::string> const& qualifyingBusinesses) {
 
-	// This function needs to find all reviews that have business_ids in
-	// the qualifyingBusinessesIDs vector and build a histogram over their stars
-	// The return value is that histogram
-	std::cout << "function aggregateStarsOfQualifyingBusinesses not implemented" << std::endl;
-	throw std::logic_error("unimplemented");
+	std::vector<unsigned long> result = {0, 0, 0, 0, 0, 0};
+	for (int i = 0; i < r.business_ids.size(); i++) 
+		if (qualifyingBusinesses.count(r.business_ids[i]))
+			result[r.stars[i]] += 1;
+
+	return result;
 }
